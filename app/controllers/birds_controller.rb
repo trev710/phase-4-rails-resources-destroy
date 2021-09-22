3ec2b344fb7ab47478b25fc0e30698e533e95fda
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-09-22 10:36:40 -0600

    monke

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -44,10 +44,22 @@ class BirdsController < ApplicationController
     end
   end
 
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
     params.permit(:name, :species, :likes)
   end
+
+  
 
 end
